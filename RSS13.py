de45d6d21ae657cca7e1ab5255dfbabe80bd22b6
aiueo0306 <sato.sota@create-sd.co.jp>
2025-09-02 12:57:01 +0900

    Update RSS13.py

--- RSS13.py
+++ RSS13.py
@@ -4,6 +4,7 @@ import subprocess
 import tempfile
 import re
 import time
+import datetime
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 # ===== GitHub 上の共通関数を一時ディレクトリにクローン =====
@@ -25,20 +26,20 @@ from scraper_utils import extract_items
 from browser_utils import click_button_in_order
 
 # ===== 固定情報（学会サイト） =====
-BASE_URL = "https://medical-pro.kaken.co.jp/index.html"
-GAKKAI = "科研製薬(その他)"
+BASE_URL = "https://medical.taisho.co.jp/medical/doctor-news/"
+GAKKAI = "大正製薬（ニュースレター）"
 
-SELECTOR_TITLE = "dl.list-information dd"
-title_selector = "a"
+SELECTOR_TITLE = "div._page_inner_contents_1r7ok_40.m_6d731127.mantine-Stack-root div._news_text_jzd1w_19"
+title_selector = ""
 title_index = 0
 href_selector = "a"
 href_index = 0
-SELECTOR_DATE = "dl.list-information dt"  # typo修正済み
-date_selector = "time"
+SELECTOR_DATE = "p.mantine-focus-auto._date_jzd1w_1.m_b6d8b162.mantine-Text-root"  # typo修正済み
+date_selector = ""
 date_index = 0
-year_unit = "年"
-month_unit = "月"
-day_unit = "日"
+year_unit = "."
+month_unit = "."
+day_unit = ""
 date_format = f"%Y{year_unit}%m{month_unit}%d{day_unit}"
 date_regex = rf"(\d{{2,4}}){year_unit}(\d{{1,2}}){month_unit}(\d{{1,2}}){day_unit}"
 # date_format = f"%Y{year_unit}%m{month_unit}%d{day_unit}"
@@ -46,7 +47,7 @@ date_regex = rf"(\d{{2,4}}){year_unit}(\d{{1,2}}){month_unit}(\d{{1,2}}){day_uni
 
 # ===== ポップアップ順序クリック設定 =====
 POPUP_MODE = 1  # 0: ポップアップ処理しない, 1: 処理する
-POPUP_BUTTONS = ["薬剤師","全て許可"] if POPUP_MODE else [] 
+POPUP_BUTTONS = ["薬剤師"] if POPUP_MODE else [] 
 WAIT_BETWEEN_POPUPS_MS = 500
 BUTTON_TIMEOUT_MS = 12000
 
@@ -90,7 +91,7 @@ with sync_playwright() as p:
             print("ℹ ポップアップ処理はスキップしました（POPUP_MODE=0 または ボタン未指定）")
 
         # 本文読み込み
-        page.wait_for_load_state("load", timeout=30000)
+        page.wait_for_load_state("load", timeout=120000)
 
     except PlaywrightTimeoutError:
         print("⚠ ページの読み込みに失敗しました。")
@@ -117,6 +118,6 @@ with sync_playwright() as p:
         print("⚠ 抽出できた記事がありません。HTML構造が変わっている可能性があります。")
 
     os.makedirs("rss_output", exist_ok=True)
-    rss_path = "rss_output/Feed8-5.xml"
+    rss_path = "rss_output/Feed20-2.xml"
     generate_rss(items, rss_path, BASE_URL, GAKKAI)
     browser.close()
